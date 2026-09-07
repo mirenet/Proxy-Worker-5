1d47a68e8716b6def54f4cd46322c97b5c4a07fb
mirenet <mireinternet@tutamail.com>
2026-09-07 15:52:01 +0200

    Change btnExport type from TextView to View

--- app/src/main/java/com/netproxy/app/MainActivity.kt
+++ app/src/main/java/com/netproxy/app/MainActivity.kt
@@ -38,7 +38,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var btnTurnOn: TextView
     private lateinit var btnTurnOff: TextView
     private lateinit var btnImport: TextView
-    private lateinit var btnExport: TextView
+    private lateinit var btnExport: View
     private lateinit var tvStatus: TextView
     private lateinit var tvExample: TextView
     private lateinit var prefs: SharedPreferences
